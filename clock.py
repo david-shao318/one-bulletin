009da55df78d8ae89891b00ed76670649ece4e5e
David Shao <57266876+david-shao318@users.noreply.github.com>
2022-06-02 18:54:17 -0600

    Comments for clock.py

--- clock.py
+++ clock.py
@@ -1,3 +1,6 @@
+# clock.py
+# sets up APScheduler for Heroku to run at set intervals
+
 import os
 from datetime import datetime
 import django
@@ -11,11 +14,14 @@ from one_bulletin.management.commands.accessfeeds import Command as AccessFeedsC
 sched = BlockingScheduler()
 
 
+# run accessfeeds command manually
 def timed_feed_access():
     cmd = AccessFeedsCommand()
     cmd.handle()
 
 
+# run this job every 15 minutes
+# perform first run right now
 sched.add_job(timed_feed_access, 'interval', minutes=15, next_run_time=datetime.now())
 
 sched.start()
